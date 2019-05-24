943a921082cb17c17a67b932eef807dbfca3ad36
18016179 <18016179@myrp.edu.sg>
2019-05-24 15:31:06 +0800

    commit

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //check in by Jerome
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
